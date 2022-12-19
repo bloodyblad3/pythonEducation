fa3d37b191770eaa600812763a1efa7a1a06abec
externalcharm <freesxsad23@mail.ru>
2022-12-19 20:16:19 +0300

    homework with task2

---
+++ seminar2/task2.py
@@ -0,0 +1,27 @@
+# 2 Напишите программу, которая принимает на вход число N и выдает набор произведений чисел от 1 до N.
+
+# Пример:
+
+# - пусть N = 4, тогда [ 1, 2, 6, 24 ] (1, 1*2, 1*2*3, 1*2*3*4)
+
+def input_number(text: str) -> int:
+    try:
+        number = float(input(text))
+    except ValueError:
+        print("это не число!")
+    return number
+
+def mult(n: int):
+    if n == 1:
+        return 1
+    else:
+        return n * mult(n - 1)
+
+
+num = input_number("Введите число: ")
+
+list = []
+for i in range(1, num + 1):
+    list.append(mult(i))
+
+print(f"Произведения чисел от 1 до {num}:  {list}")
