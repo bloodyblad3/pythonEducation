e24357c76d4f94bb07b37a69d01e47ec66acb417
externalcharm <freesxsad23@mail.ru>
2022-12-20 11:54:48 +0300

    homework with task4

--- seminar3/task4.py
+++ seminar3/task4.py
@@ -3,3 +3,20 @@
 # - 45 -> 101101
 # - 3 -> 11
 # # - 2 -> 10
+
+def input_number(text: str) -> int:
+    try:
+        number = int(input(text))
+    except ValueError:
+        print("это не число!")
+    return number
+
+def to_binary(num: int):
+    s = ""
+    while num != 0:
+        s = str(num%2) + s
+        num //= 2
+    return s
+
+num = input_number("Введите число для преобразования: ")
+print(f"Число {num} в двоичном виде > {to_binary(num)}")
