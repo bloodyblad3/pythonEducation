a8aa8ee5da7840af0dfb9b4bf02aaa386441e65d
externalcharm <freesxsad23@mail.ru>
2022-12-22 20:55:21 +0300

    homework with task2

--- seminar4/task2.py
+++ seminar4/task2.py
@@ -1,1 +1,24 @@
 # Задайте натуральное число N. Напишите программу, которая составит список простых множителей числа N.
+
+def input_number(text: str) -> int:
+    try:
+        number = int(input(text))
+    except ValueError:
+        print("это не число!")
+    return number
+
+def prime_factors(num: int) -> list:
+    list = []
+    i = 2
+    while i <= num:
+        if num % i == 0:
+            list.append(i)
+            num //= i
+            i = 2
+        else:
+            i += 1
+    return list
+
+num = input_number("Введите число: ")
+factors = prime_factors(num)
+print(f"Простые множители числа {num} > {factors}")
