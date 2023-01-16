7224a888cbd2ddcdbaf71058da90b222648991e6
externalcharm <freesxsad23@mail.ru>
2023-01-16 22:55:59 +0300

    homework with task2

--- seminar6/task2.py
+++ seminar6/task2.py
@@ -2,3 +2,11 @@
 # *Пример:*
 # - A (3,6); B (2,1) -> 5,09
 # - A (7,-5); B (1,-1) -> 7,21
+
+def count_distance():
+    point_a = [int(input(f"Введите {i} точки A: ")) for i in 'xy']
+    point_b = [int(input(f"Введите {i} точки B: ")) for i in 'xy'] 
+    return sum((point[0] - point[1])**2 for point in zip(point_a, point_b))**0.5
+
+sum = count_distance()
+print(f"{sum:.2f}")
