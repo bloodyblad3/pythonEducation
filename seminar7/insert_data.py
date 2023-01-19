579d4a286313ee381b499a941f4a45f111e7f94b
externalcharm <freesxsad23@mail.ru>
2023-01-19 22:54:01 +0300

    renamed file and added a flag

--- seminar7/insert_data.py
+++ seminar7/insert_data.py
@@ -1,5 +1,5 @@
 def insert_data(data, sep=None):
-    with open("phone.txt") as file:
+    with open("note.txt", 'a+') as file:
         if sep == None:
             for i in data:
                 file.write(f"{i}\n")
