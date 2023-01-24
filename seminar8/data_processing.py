7d641be083f34d4f34af766044fa1043e01ffeaf
externalcharm <freesxsad23@mail.ru>
2023-01-24 23:04:11 +0300

    this module is response for data processing (data separation, counting id)

---
+++ seminar8/data_processing.py
@@ -0,0 +1,9 @@
+def count_ids(name):
+    with open(name, 'r') as file:
+        data = file.read()
+    return data.count('\n')
+
+def write_data(data, name):
+    with open(name, 'a+') as file:
+        file.write(";".join(map(str, data)))
+        file.write(f"\n")
