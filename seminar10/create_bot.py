00d09020769630c5e556d313c9f5346714267edb
externalcharm <freesxsad23@mail.ru>
2023-01-30 23:22:48 +0300

    structure of bot

--- seminar10/create_bot.py
+++ seminar10/create_bot.py
@@ -5,6 +5,5 @@ from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
 load_dotenv()
 
-storage = MemoryStorage()
 bot = Bot(os.getenv("TOKEN"))
-dp = Dispatcher(bot, storage=storage)
+dp = Dispatcher(bot)
